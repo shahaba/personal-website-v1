56e1a63bde93eedd501af9020eac5671f6919c93
Shahab Akmal <6495706+shahaba@users.noreply.github.com>
2023-02-25 16:40:41 -0500

    Gatsby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,19 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Personal Website v1`,
+    title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ]
 }
