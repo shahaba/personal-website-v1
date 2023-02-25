176a1a75f712e3186f4af1ef2c9db546b419463c
Shahab Akmal <6495706+shahaba@users.noreply.github.com>
2023-02-25 13:32:17 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Personal Website v1`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
